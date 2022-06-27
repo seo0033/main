341eefdef427ed752afee5e2045ce0da61d92a3b
seo0033 <tkrhk9511@gmail.com>
2022-06-27 17:47:16 +0900

    20220627

--- project02/js/main.js
+++ project02/js/main.js
@@ -8,9 +8,9 @@ $(function () {
     $('.main_arrow i:nth-child(1)').on('click', function () {
         $('.main_slider').slick('slickPrev')
     })
-    $('.main_arrow i:nth-child(2)').on('click', function () {
+    $('.main_arrow i:nth-child(3)').on('click', function () {
         $('.main_slider').slick('slickNext')
     })
-    //❓ 슬릭작동X
+    //❓ 슬릭작동
 
 })//the end
