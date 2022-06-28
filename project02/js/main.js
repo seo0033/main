66e7f819eabf2046614217976f0cbbd146998d68
seo0033 <tkrhk9511@gmail.com>
2022-06-28 18:29:31 +0900

    20220628

--- project02/js/main.js
+++ project02/js/main.js
@@ -1,6 +1,8 @@
 $(function () {
     $('.main_slider').slick({
         arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
@@ -10,7 +12,12 @@ $(function () {
     })
     $('.main_arrow i:nth-child(3)').on('click', function () {
         $('.main_slider').slick('slickNext')
-    })
-    //❓ 슬릭작동
+    });
+
+    $('.main_arrow li>a').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $('.main_slider').slick('slickGoTo', idx)
+    });
 
 })//the end
