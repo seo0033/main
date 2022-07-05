8a45088db41bda8a26f96e88c0d739e98d59f22a
seo0033 <tkrhk9511@gmail.com>
2022-07-05 17:51:46 +0900

    20220705

--- project03/js/main.js
+++ project03/js/main.js
@@ -1,24 +1,38 @@
 $(function () {
-    $('.main_slider').slick({
-        arrows: false,
-        dots: true,
-        // autoplay:true,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-    });
+  $('.main_slider').slick({
+    arrows: false,
+    dots: true,
+    // autoplay:true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
 
-    $('.slider_best_menu_top').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider_best_menu_bottom'
-      });
-      $('.slider_best_menu_bottom').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider_best_menu_top',
-        centerMode: true,
-        focusOnSelect: true
-      });
-})
+  $('.slider_best_menu_top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider_best_menu_bottom'
+  });
+  $('.slider_best_menu_bottom').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider_best_menu_top',
+    focusOnSelect: true,
+    arrows: false,
+    centerMode: true,
+  });
+
+  $('.bottom .slider_bottom_arrows i:nth-child(1)').on('click', function (e) {
+    e.preventDefault();
+    $('.slider_best_menu_bottom').slick('slickPrev')
+  })
+  $('.bottom .slider_bottom_arrows i:nth-child(2)').on('click', function (e) {
+    e.preventDefault();
+    $('.slider_best_menu_bottom').slick('slickNext')
+  });
+
+
+
+
+})//the end
