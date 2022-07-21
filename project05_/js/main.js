d2c6866bb61306d910254cc52fb17dc962b6fea8
seo0033 <tkrhk9511@gmail.com>
2022-07-21 17:49:30 +0900

    20220721

--- project05_/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    //top_banner
-    $('.top_banner a>i').on('click', function (e) {
-        e.preventDefault();
-        $('.top_banner').slideUp();
-    });
-})//the end
---
+++ project05_kaareklint/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    //top_banner
+    $('.top_banner a>i').on('click', function (e) {
+        e.preventDefault();
+        $('.top_banner').slideUp();
+    });
+
+    //show_room
+    // $('.show_room').slick();
+})//the end
