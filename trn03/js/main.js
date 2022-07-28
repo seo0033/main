2ea60c6c7758d65d32f6a824915440dce1a86c80
seo0033 <tkrhk9511@gmail.com>
2022-07-28 16:07:50 +0900

    20220728

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -3,16 +3,5 @@ $(function () {
         $('.TopBanner').slideUp();
     });
 
-    // $('.pop button').on('click', function () {
-    //     $(this).parent().hide();
-    // });
-    //팝업창 하나씩 닫기
-
-
-    $('.pop button').on('click', function () {
-        $('.pop').slideUp();
-    });
-    //팝업창 한번에 닫기
-
 
 })//the end
---
+++ trn03/popup/popup.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.pop button').on('click', function () {
+        $(this).parent().hide();
+    });
+    //팝업창 하나씩 닫기
+
+
+    // $('.pop button').on('click', function () {
+    //     $('.pop').slideUp();
+    // });
+    //팝업창 한번에 닫기
+
+})//the end
+
