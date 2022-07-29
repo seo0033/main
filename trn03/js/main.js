f3eb3930a3cd215c6ffe07809706d54b0a95f647
seo0033 <tkrhk9511@gmail.com>
2022-07-29 15:16:45 +0900

    20220729

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -3,5 +3,15 @@ $(function () {
         $('.TopBanner').slideUp();
     });
 
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // speed: 1000,
+        // cssEase: 'linear',
+        // variableWidth: true,
+    });
+
 
 })//the end
