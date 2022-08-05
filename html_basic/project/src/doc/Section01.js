a5705a74cf34b0c6e3f0d697055ae06cefa5c8a4
seo0033 <tkrhk9511@gmail.com>
2022-08-05 16:07:53 +0900

    20220805

--- html_basic/project/src/doc/Section01.js
+++ html_basic/project/src/doc/Section01.js
@@ -1,8 +1,6 @@
-import '../css/main.css'
-
 const Section01 = () => {
     return (
-        <section className="about">
+        <section className="about basicSection">
             <h2><span className="mainColor">CEO's</span> GREETINGS</h2>
             <p>새로운 미래를 열어가는 우tothe영tothe우</p>
             <div className="container">
