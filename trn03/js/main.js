9f7f66ff0cb998ef92b4f8cdc28682752e9d7702
seo0033 <tkrhk9511@gmail.com>
2022-08-08 17:45:59 +0900

    20220808

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -1,9 +1,14 @@
 $(function () {
-    $('.TopBanner i').on('click', function () {
-        $('.TopBanner').slideUp();
-    });
 
-    $('.main_slider').slick({
+
+    var closeBanner = () => {
+        $('.TopBanner').slideToggle('on')
+        // document.querySelector('.TopBanner').classList.toggle('on')
+    }
+
+    $('.TopBanner i').on('click', closeBanner);
+
+    $('.MainSlider').slick({
         arrows: false,
         dots: true,
         autoplay: true,
@@ -12,6 +17,29 @@ $(function () {
         // cssEase: 'linear',
         // variableWidth: true,
     });
+
+    $('.ProductSlider').slick({
+        arrows: false,
+        slidesToShow: 5,
+        responsive: [
+            {
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+    //e.currentTarget
+
+    function toggleClass() {
+        $(this).toggleClass('on')
+        $('.Footer .popup').toggleClass('on')
+    }
+    $('.Footer .pop li').on('click', toggleClass);
+    $('.popup i').on('click', toggleClass);
 
 
 })//the end
