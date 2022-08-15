fe7c3ba591ca49e02b64f4f627311980171310a0
seo0033 <tkrhk9511@gmail.com>
2022-08-16 00:32:57 +0900

    20220816

--- project07_namuya/js/main.js
+++ project07_namuya/js/main.js
@@ -5,10 +5,23 @@ $(function () {
         // autoplay: true, 
         pauseOnHover: false,
         pauseOnFocus: false,
-        autoplaySpeed: 3000,
     });
 
     $('.main_visual .main_slider').on('init afterChange', function (e, s, c) {
         $('.main_slider figure.slick-current ').addClass('on').siblings().removeClass('on');
-    });
+    });//main_visual
+
+    $('.menu .menu_slider').slick()
+
+    $('.tab_menu>li>a').on('click', function (e) {
+        e.preventDefault();
+        // console.log($(this).parent().index());
+        var idx = $(this).parent().index();
+        $('.tab_content>li').removeClass('on');
+        $('.tab_content>li').eq(idx).addClass('on');
+        $('.tab_menu>li').removeClass('on');
+        $(this).parent().addClass('on')
+
+    })
+
 })//the end
