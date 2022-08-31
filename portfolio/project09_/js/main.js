3a5c90e1fc1415e7c4cd80a3d3d124c531bb0a87
seo0033 <tkrhk9511@gmail.com>
2022-08-31 17:50:46 +0900

    20220831

--- portfolio/project09_/js/main.js
+++ portfolio/project09_/js/main.js
@@ -13,4 +13,44 @@ window.addEventListener('DOMContentLoaded', () => {
         },
     });
 
+    const Card_slider = new Swiper(".card_slider", {
+        effect: "cards",
+        grabCursor: true,
+    });
+
+    Card_slider.on('slideChangeTransitionEnd', () => {
+        //console.log(Card_slider.realIndex);
+        let rightBox = document.querySelectorAll('.character .right .box');
+        //console.log(rightBox);
+        rightBox.forEach((itm) => {
+            itm.classList.remove('on')
+        })
+        rightBox[Card_slider.realIndex].classList.add('on');
+        //let current = document.querySelector('.swiper-slide-active');
+    });
+
+    const Ost_slider = new Swiper(".ost_slider", {
+        effect: "coverflow",
+        // grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+    });
+
+    Ost_slider.on()
+
+
+
+
+
 });// the end
